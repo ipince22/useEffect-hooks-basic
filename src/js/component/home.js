43b1f4e09ba1750aebdf3c971433134c6b178106
Daniel Ipince <ipince22@gmail.com>
2021-05-18 02:45:26 +0000

    useeffect

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [resourceType, setresourceType] = useState("posts");
+	const [items, setItems] = useState([]);
+
+	console.log("render");
+	useEffect(() => {
+		fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+			.then(response => response.json())
+			.then(json => setItems(json));
+	}, [resourceType]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="text-center mt-5">
+				<button onClick={() => setresourceType("posts")}>Posts</button>
+				<button onClick={() => setresourceType("users")}>Users</button>
+				<button onClick={() => setresourceType("comments")}>
+					Comments
+				</button>
+			</div>
+			<h1>{resourceType}</h1>
+			{items.map((item, index) => {
+				return <pre key={index}>{JSON.stringify(item)}</pre>;
+			})}
+		</>
 	);
 }
